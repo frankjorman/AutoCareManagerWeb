6bcf249a365b61626ec00aeca02bd5835e9e8f40
frank <frankjorman@hotmail.com>
2023-10-23 20:28:50 -0500

    Inicio

---
+++ src/pages/EmpleadoMantenimiento.vue
@@ -0,0 +1,32 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Employee Name</th>
+        <th>Employee ID</th>
+        <th>Department</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(employee, index) in employees" :key="index">
+        <td>{{ employee.name }}</td>
+        <td>{{ employee.id }}</td>
+        <td>{{ employee.department }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      employees: [
+        { name: "John Doe", id: 1234, department: "Sales" },
+        { name: "Jane Smith", id: 5678, department: "Marketing" },
+        { name: "Bob Johnson", id: 9012, department: "Finance" },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/pages/LoginInicio.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page>
+    <q-page-container>
+      <q-card class="q-pa-md">
+        <q-form @submit.prevent="login">
+          <q-input v-model="username" label="Username" />
+          <q-input v-model="password" label="Password" type="password" />
+          <q-btn type="submit" label="Login" class="q-mt-md" />
+        </q-form>
+      </q-card>
+    </q-page-container>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // Aquí puedes agregar la lógica para enviar el formulario de inicio de sesión
+    },
+  },
+};
+</script>
